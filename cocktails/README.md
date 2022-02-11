6a6f6f5663411a769e2a97f535c23159fe94544a
niemante <96666179+niemante@users.noreply.github.com>
2022-02-11 16:06:53 +0100

    Add files via upload

---
+++ cocktails/README.md
@@ -0,0 +1,1 @@
+Project from John Smilga course on React
---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, natus eum
+        quae ducimus aliquam incidunt tempora quasi ratione dolorem iure illum
+        nemo, possimus dicta dolorum velit, officia minus eveniet dolores earum
+        vitae voluptatem. Fugit dignissimos laudantium dolorem, eum quas non
+        aliquid sequi, adipisci, consequuntur esse corporis enim? Amet ipsam ex
+        possimus, excepturi dicta, praesentium libero esse illo veniam repellat,
+        sapiente quae quidem vero facilis quo minima distinctio rem perspiciatis
+        blanditiis ducimus quasi ratione voluptatibus. Excepturi vitae
+        asperiores quae unde natus?
+      </p>
+    </section>
+  );
+};
+
+export default About;
